2ec88becfd05f6393c77c13f445d86ad494ba53b
Andrei <kadrov2013@yandex.ru>
2022-09-03 17:02:34 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число: "); //Запрос ввести число пользователя
+int firstnumber = Convert.ToInt32(Console.ReadLine()); //Присвоили значениее переменной (Convert.ToInt32)-инвертировали строку в int
+firstnumber *= firstnumber; //Умножение переменной саму на себя(4*4)
+Console.WriteLine(firstnumber); //Вывод результата в терминале
+
