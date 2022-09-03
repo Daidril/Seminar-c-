0038f3c444458d2f5e5ddde33906d2611acb643e
Andrei <kadrov2013@yandex.ru>
2022-09-03 18:47:09 +0300

    First day

---
+++ Task3/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Введите номер дня, от 1 до 7: ");
+int firstday = Convert.ToInt32(Console.ReadLine());
+
+//Выводим день недели 
+switch(firstday)
+{
+    case  1:
+    Console.WriteLine("Понедельник");
+    break;
+
+    case  2:
+    Console.WriteLine("Вторник");
+    break;
+
+    case  3:
+    Console.WriteLine("Среда");
+    break;
+
+    case  4:
+    Console.WriteLine("Четверг");
+    break;
+
+    case  5:
+    Console.WriteLine("Пятница");
+    break;
+
+    case  6:
+    Console.WriteLine("Суббота");
+    break;
+
+    case  7:
+    Console.WriteLine("Воскресение");
+    break;
+
+    default:
+    Console.WriteLine("Число не входит в диапозон");
+    break;
+}
