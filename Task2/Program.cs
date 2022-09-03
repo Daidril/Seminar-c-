6ce9a8ff8f013132ed3b33e9d4d2dd44202bfba7
Andrei <kadrov2013@yandex.ru>
2022-09-03 18:19:49 +0300

    Kvadrat Chisla

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число: ");
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int secondnumber = Convert.ToInt32(Console.ReadLine());
+//Proverka na yslovie
+if (firstnumber * firstnumber == secondnumber)
+{
+    Console.WriteLine("Является квадратом первого числа");
+}
+else
+{
+    Console.WriteLine("Неявляется квадратом первого числа");
+}
