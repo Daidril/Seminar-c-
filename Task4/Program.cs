3a0ae7f5969641cc3303a258495a7c810297f531
Andrei <kadrov2013@yandex.ru>
2022-09-03 20:13:33 +0300

    Bolshee, menshee

---
+++ Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число: ");
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int secondnumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstnumber > secondnumber)
+{
+    Console.WriteLine("Первое число больше, второе меньшне");
+}
+else
+{
+    Console.WriteLine("Второе число больше, первое меньше");
+}
+
