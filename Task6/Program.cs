96d28570346b3cac47ad7686b051a645c4bfa6cf
Andrei <kadrov2013@yandex.ru>
2022-09-03 22:35:24 +0300

    Parity

---
+++ Task6/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число: ");
+int numb = Convert.ToInt32(Console.ReadLine());
+
+if (numb % 2 == 0) //Проверка числа на четность.
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Нечетное");
+}
+
+
+
+
