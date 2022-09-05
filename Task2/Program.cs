992b4c9647acd95daa20cdd53970a6b9b4f3c06d
Andrei <kadrov2013@yandex.ru>
2022-09-05 14:15:28 +0300

    task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,13 +1,15 @@
 ﻿Console.WriteLine("Введите первое число: ");
 int firstnumber = Convert.ToInt32(Console.ReadLine());
+
 Console.WriteLine("Введите второе число");
 int secondnumber = Convert.ToInt32(Console.ReadLine());
+
 //Proverka na yslovie
 if (firstnumber * firstnumber == secondnumber)
 {
-    Console.WriteLine("Является квадратом первого числа");
+    Console.WriteLine("Является квадратом второго числа");
 }
 else
 {
-    Console.WriteLine("Неявляется квадратом первого числа");
+    Console.WriteLine("Неявляется квадратом второго числа");
 }
