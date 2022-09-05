ed01fe5647d2bf7febed36b2d53b9a52658ae9ff
Andrei <kadrov2013@yandex.ru>
2022-09-05 14:17:55 +0300

    task8

---
+++ Task8/Program.cs
@@ -0,0 +1,9 @@
+﻿//Вывести числа от -N до N
+Console.WriteLine("Enter number");
+int number = Convert.ToInt32(Console.ReadLine());
+int minusNumber = -number;
+while(minusNumber <= number)
+{
+    Console.WriteLine(minusNumber);
+    minusNumber++;
+}
