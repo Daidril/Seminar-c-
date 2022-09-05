8b5b4f4b243fa18407d28e485dde66e47ac1ff92
Andrei <kadrov2013@yandex.ru>
2022-09-05 14:16:58 +0300

    task5

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Введите первое число: ");
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите первое число: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите второе число: ");
