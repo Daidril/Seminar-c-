c167102bb736095033219473258b4baf7361a2df
Andrei <kadrov2013@yandex.ru>
2022-09-07 14:27:31 +0300

    task7

---
+++ Task7/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите число: ");
+int numb1 = Convert.ToInt32(Console.ReadLine());
+for( int i = 2; i <= numb1; i++)
+{
+    if(i % 2 ==0)
+
+    {
+        Console.WriteLine(i);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
